bbd5212368c95b5d6bee4254cf064593b8bc7aea
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2025-12-17 11:33:51 +0100

    Adding slider for h

--- main.cpp
+++ main.cpp
@@ -20,6 +20,127 @@ using namespace DGtal;
 typedef Shortcuts<Z3i::KSpace>         SH3;
 typedef ShortcutsGeometry<Z3i::KSpace> SHG3;
 
+//Global variables for UI
+float gridstep = 0.5;
+string polynomial = "ellipsoid";
+vector<string> polynomialList = { "ellipsoid", "rcube", "sphere9" };
+
+//Main loop
+void go()
+{
+  // parameters
+  auto params    = SH3::defaultParameters() | SHG3::defaultParameters();
+  params( "polynomial", polynomial )( "gridstep", gridstep );
+  
+  //----------------------------------------------------------------------------
+  trace.beginBlock("digital surface extraction");
+  auto implicit_shape  = SH3::makeImplicitShape3D( params );
+  auto digitized_shape = SH3::makeDigitizedImplicitShape3D( implicit_shape, params );
+  auto K               = SH3::getKSpace( params );
+  auto surfacePtr      = SH3::makeDigitalSurface( digitized_shape, K, params );
+  
+  // surface as mesh
+  SurfaceManager<Z3i::KSpace> surface;
+  surface.buildMesh(K, surfacePtr);
+  trace.endBlock();
+  
+  //----------------------------------------------------------------------------
+  trace.beginBlock("convex hull computation");
+  ConvexHullManager<Z3i::KSpace> convexHull;
+  convexHull.build(surface.myVertices);
+  trace.endBlock();
+  
+  //----------------------------------------------------------------------------
+  trace.beginBlock("Expected normals...");
+  auto expectedNormals = SHG3::getNormalVectors( implicit_shape, K, SH3::getSurfelRange(surfacePtr), params );
+  trace.endBlock();
+  
+  //----------------------------------------------------------------------------
+  trace.beginBlock("normal estimation");
+  set<Z3i::Cell> out, in;
+  vector<Z3i::RealVector> normals(surface.mySurfels.size(),
+                                  Z3i::Vector::diagonal(0.0));
+  
+  for (int k = 0; k < convexHull.myFaces.size(); ++k) {
+    //trace.info() << "# Face #" << k << endl;
+    //face vertices
+    vector<Z3i::Point> faceVertices;
+    for (auto const& idx: convexHull.myFaces.at(k)) {
+      faceVertices.push_back( convexHull.myVertices[idx] );
+    }
+    DigitizedCvxPolygon<Z3i::KSpace> cp(K, faceVertices);
+    //face normal
+    Z3i::RealVector n = cp.getUnitNormal();
+    //face digitization
+    set<Z3i::Cell> surfelSet;
+    cp.digitize(inserter(surfelSet, surfelSet.end()));
+    //trace.info() << "# " << surfelSet.size() << " cells" << endl;
+    
+    //assign a normal to every surfel in the surface
+    for (auto const& s: surfelSet) {
+      auto it = surface.mySMap.find(s);
+      if (it != surface.mySMap.end()) { //if in surface
+        in.insert(s);
+        Z3i::RealVector sum = normals.at(it->second) + n;
+        normals.at(it->second) = sum.getNormalized();
+      } else {
+        out.insert(s);
+      }
+    }
+  }
+  trace.info() << "# " << in.size() << " cells / "
+  << surface.mySurfels.size() << " recovered" << endl;
+  trace.endBlock();
+  
+  //----------------------------------------------------------------------------
+  trace.beginBlock("Statistics...");
+  ASSERT(normals.size() == expectedNormals.size());
+  auto diff = SHG3::getVectorsAngleDeviation(expectedNormals, normals);
+  auto stat = SHG3::getStatistic(diff);
+  
+  trace.info() << "#gridstep nbVtx min avg max stdVar" << endl;
+  trace.info() << gridstep << " "
+  << normals.size() << " "
+  << stat.min() << " "
+  << stat.mean() << " "
+  << stat.max() << " "
+  << sqrt((double) stat.variance()) << " "
+  << endl;
+  trace.endBlock();
+  
+  //----------------------------------------------------------------------------
+  // polyscope session
+  polyscope::options::groundPlaneMode = polyscope::GroundPlaneMode::None;
+  
+  //structures
+  auto sPtr = polyscope::registerSurfaceMesh("Surface",
+                                             surface.myVertices,
+                                             surface.myFaces);
+  sPtr->setEdgeWidth(1.0);
+  
+  auto cPtr = polyscope::registerSurfaceMesh("Convex hull",
+                                             convexHull.myVertices,
+                                             convexHull.myFaces);
+  cPtr->setEdgeWidth(2.0);
+  cPtr->setTransparency(0.75);
+  cPtr->setEnabled(false);
+  
+  //quantities
+  sPtr->addFaceVectorQuantity("Estimated Normals", normals);
+  sPtr->addFaceVectorQuantity("Expected Normals", expectedNormals);
+  auto dPtr = sPtr->addFaceScalarQuantity("Deviation", diff);
+  dPtr->setEnabled(true);
+  
+}
+
+//UI
+void myCallback()
+{
+  ImGui::SliderFloat("h", &gridstep, 0.0, 1.0);
+  if (ImGui::Button("Run"))
+    go();
+}
+
 //--------------------------------------------------------------------------------
 //-- main 
 //--------------------------------------------------------------------------------
@@ -30,10 +151,6 @@ int main (int argc, char* argv[]) {
   
   app.description("Demo: 'Geometry of Gauss digitized convex shapes'\n");
 
-  double gridstep = 0.5;
-  string polynomial = "ellipsoid";
-  
-  vector<string> polynomialList = { "ellipsoid", "rcube", "sphere9" }; 
   stringstream ss;
   ss << "A polynomial";
   if (polynomialList.size() > 0) {
@@ -53,111 +170,10 @@ int main (int argc, char* argv[]) {
   CLI11_PARSE(app, argc, argv);
   // END parse command line using CLI
 
-  // parameters
-  auto params    = SH3::defaultParameters() | SHG3::defaultParameters();
-  params( "polynomial", polynomial )( "gridstep", gridstep );
-  
-  //---------------------------------------------------------------------------- 
-  trace.beginBlock("digital surface extraction"); 
-  auto implicit_shape  = SH3::makeImplicitShape3D( params );
-  auto digitized_shape = SH3::makeDigitizedImplicitShape3D( implicit_shape, params );
-  auto K               = SH3::getKSpace( params );
-  auto surfacePtr      = SH3::makeDigitalSurface( digitized_shape, K, params );
-
-  // surface as mesh
-  SurfaceManager<Z3i::KSpace> surface;
-  surface.buildMesh(K, surfacePtr);
-  trace.endBlock();
-
-  //---------------------------------------------------------------------------- 
-  trace.beginBlock("convex hull computation"); 
-  ConvexHullManager<Z3i::KSpace> convexHull;
-  convexHull.build(surface.myVertices);
-  trace.endBlock();
-
-  //---------------------------------------------------------------------------- 
-  trace.beginBlock("Expected normals...");
-  auto expectedNormals = SHG3::getNormalVectors( implicit_shape, K, SH3::getSurfelRange(surfacePtr), params );
-  trace.endBlock();
-  
-  //---------------------------------------------------------------------------- 
-  trace.beginBlock("normal estimation");
-  set<Z3i::Cell> out, in; 
-  vector<Z3i::RealVector> normals(surface.mySurfels.size(),
-				  Z3i::Vector::diagonal(0.0));
-  
-  for (int k = 0; k < convexHull.myFaces.size(); ++k) {
-    //trace.info() << "# Face #" << k << endl; 
-    //face vertices
-    vector<Z3i::Point> faceVertices;
-    for (auto const& idx: convexHull.myFaces.at(k)) {
-      faceVertices.push_back( convexHull.myVertices[idx] ); 
-    }
-    DigitizedCvxPolygon<Z3i::KSpace> cp(K, faceVertices);
-    //face normal
-    Z3i::RealVector n = cp.getUnitNormal(); 
-    //face digitization
-    set<Z3i::Cell> surfelSet;
-    cp.digitize(inserter(surfelSet, surfelSet.end()));
-    //trace.info() << "# " << surfelSet.size() << " cells" << endl; 
-
-    //assign a normal to every surfel in the surface
-    for (auto const& s: surfelSet) {
-      auto it = surface.mySMap.find(s); 
-      if (it != surface.mySMap.end()) { //if in surface
-	in.insert(s); 
-	Z3i::RealVector sum = normals.at(it->second) + n;
-	normals.at(it->second) = sum.getNormalized(); 
-      } else {
-	out.insert(s); 
-      }
-    }
-  }
-  trace.info() << "# " << in.size() << " cells / "
-	       << surface.mySurfels.size() << " recovered" << endl; 
-  trace.endBlock();
-
-  //---------------------------------------------------------------------------- 
-  trace.beginBlock("Statistics..."); 
-  ASSERT(normals.size() == expectedNormals.size());
-  auto diff = SHG3::getVectorsAngleDeviation(expectedNormals, normals);
-  auto stat = SHG3::getStatistic(diff);
-
-  trace.info() << "#gridstep nbVtx min avg max stdVar" << endl; 
-  trace.info() << gridstep << " "
-	       << normals.size() << " "
-	       << stat.min() << " "
-	       << stat.mean() << " "
-	       << stat.max() << " "
-	       << sqrt((double) stat.variance()) << " "
-	       << endl;
-  trace.endBlock(); 
-
-  //---------------------------------------------------------------------------- 
-  // polyscope session
   PolyscopeViewer viewer;
-  polyscope::options::groundPlaneMode = polyscope::GroundPlaneMode::None;
-
-  //structures
-  auto sPtr = polyscope::registerSurfaceMesh("Surface",
-					     surface.myVertices,
-					     surface.myFaces);
-  sPtr->setEdgeWidth(1.0);
-
-  auto cPtr = polyscope::registerSurfaceMesh("Convex hull",
-					     convexHull.myVertices,
-					     convexHull.myFaces);
-  cPtr->setEdgeWidth(2.0);
-  cPtr->setTransparency(0.75); 
-  cPtr->setEnabled(false); 
-
-  //quantities
-  sPtr->addFaceVectorQuantity("Estimated Normals", normals); 
-  sPtr->addFaceVectorQuantity("Expected Normals", expectedNormals);
-  auto dPtr = sPtr->addFaceScalarQuantity("Deviation", diff);
-  dPtr->setEnabled(true);
-
+  polyscope::state::userCallback = myCallback; // specify the callback
+  go(); //mainloop
   viewer.show();
-  
+
   return 0;
 }
